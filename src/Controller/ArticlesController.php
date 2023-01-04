8eaf36142e6d0785787ddf0278bfaaec668bc782
krahulteq <krahul@teqmavens>
2023-01-04 11:13:10 +0530

    **

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -9,33 +9,33 @@ class ArticlesController extends AppController
     public function initialize(): void
     {
         parent::initialize();
-
+        
         $this->loadComponent('Paginator');
         $this->loadComponent('Flash'); // Include the FlashComponent
     }
-
+    
     public function index()
     {
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
-
+    
     public function view($slug)
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
     }
-
+    
     public function add()
     {
         $article = $this->Articles->newEmptyEntity();
         if ($this->request->is('post')) {
             $article = $this->Articles->patchEntity($article, $this->request->getData());
-
+            
             // Hardcoding the user_id is temporary, and will be removed later
             // when we build authentication out.
             $article->user_id = 1;
-
+            
             if ($this->Articles->save($article)) {
                 $this->Flash->success(__('Your article has been saved.'));
                 return $this->redirect(['action' => 'index']);
@@ -44,13 +44,13 @@ class ArticlesController extends AppController
         }
         $this->set('article', $article);
     }
-
+    
     public function edit($slug)
     {
         $article = $this->Articles
-            ->findBySlug($slug)
-            ->firstOrFail();
-
+        ->findBySlug($slug)
+        ->firstOrFail();
+        
         if ($this->request->is(['post', 'put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
@@ -59,14 +59,14 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to update your article.'));
         }
-
+        
         $this->set('article', $article);
     }
-
+    
     public function delete($slug)
     {
         $this->request->allowMethod(['post', 'delete']);
-
+        
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         if ($this->Articles->delete($article)) {
             $this->Flash->success(__('The {0} article has been deleted.', $article->title));
@@ -74,3 +74,7 @@ class ArticlesController extends AppController
         }
     }
 }
+
+// echo "<pre>";
+// print_r($article);
+// die;
